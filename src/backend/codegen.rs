77f3e36466ba004e72a4aa5034e3f094ffbed69b
richard-blazek <moi@richardblazek.com>
2025-08-31 05:06:05 +0200

    WASM gen for module

--- src/backend/codegen.rs
+++ src/backend/codegen.rs
@@ -6,12 +6,43 @@ macro_rules! fmt {
     }
 }
 
-fn gen_func(s: &mut String, name: String, code: Vec<Instr>, args: Vec<Type>, ret: Type) {
+fn type_to_str(tpe: &Type) -> &str {
+    match tpe {
+        Type::Int => "i64",
+        Type::Real => "f64",
+        _ => "i32",
+    }
+}
+
+fn gen_instr(s: &mut String, instr: Instr) {
 
 }
 
-fn gen_program(s: &mut String, funcs: Vec<Func>, entry: usize) {
+fn gen_func(s: &mut String, i: usize, code: Vec<Instr>, args: Vec<Type>, ret: Type) {
+    fmt!(s, "(func $func_{} (param", i);
+    for arg in &args {
+        fmt!(s, " {}", type_to_str(arg));
+    }
+    fmt!(s, " i32) (result {})\n", type_to_str(&ret));
 
+    for instr in code {
+        gen_instr(s, instr);
+    }
+    fmt!(s, ")\n");
+
+    fmt!(s, "elem (i32.const {}) $func_{})\n", i, i);
+}
+
+fn gen_program(s: &mut String, funcs: Vec<Func>, entry: usize) {
+    fmt!(s, "(module\n");
+    fmt!(s, "(table $table {} funcref)\n", funcs.len());
+
+    for (i, func) in funcs.into_iter().enumerate() {
+        gen_func(s, i, func.code, func.args, func.ret);
+    }
+
+    fmt!(s, "(start $func_{})\n", entry);
+    fmt!(s, ")\n");
 }
 
 pub fn generate(program: Program) -> String {
