ca8e2cf750032b220a07114a862baa1a4bdca25c
richard-blazek <moi@richardblazek.com>
2025-08-31 16:33:33 +0200

    Tuple wasmgen

--- src/backend/wasmgen.rs
+++ src/backend/wasmgen.rs
@@ -15,49 +15,61 @@ fn type_to_str(tpe: &Type) -> &str {
 }
 
 fn gen_text_literal(s: &mut String, text: String) {
-    todo!()
+    let mut bytes = text.into_bytes();
+    bytes.resize(bytes.len() + (4 - bytes.len() % 4), 0);
+
+    fmt!(s, "(global.set $handy1 (call $malloc (i32.const {})))", bytes.len());
+    for (i, byte) in bytes.iter().enumerate() {
+        fmt!(s, "(i32.store8 (i32.const {}) (i32.add (global.get $handy1) (i32.const {})))", byte, i);
+    }
+    fmt!(s, "(global.get $handy1)");
 }
 
 fn gen_instr(s: &mut String, instr: Instr) {
     match instr {
-        Instr::PushInt { value } => fmt!(s, "i64.const {}\n", value),
-        Instr::PushReal { value } => fmt!(s, "f64.const {}\n", value),
+        Instr::PushInt { value } => fmt!(s, "(i64.const {})", value),
+        Instr::PushReal { value } => fmt!(s, "(f64.const {})", value),
         Instr::PushText { value } => gen_text_literal(s, value),
-        Instr::PushBool { value } => fmt!(s, "i32.const {}\n", value as i32),
-        Instr::Drop { .. } => fmt!(s, "drop\n"),
+        Instr::PushBool { value } => fmt!(s, "(i32.const {})", value as i32),
+        Instr::Drop { .. } => fmt!(s, "(drop)"),
         Instr::Block { id, inner } => {
-            fmt!(s, "(block $block_{}\n", id);
-            fmt!(s, "(loop $loop_{}\n", id);
+            fmt!(s, "(block $block_{} ", id);
+            fmt!(s, "(loop $loop_{} ", id);
             gen_instrs(s, inner);
-            fmt!(s, ")\n)\n");
+            fmt!(s, "))");
         }
-        Instr::RepeatBlock { id } => fmt!(s, "br $loop_{}\n", id),
-        Instr::QuitBlock { id } => fmt!(s, "br $block_{}\n", id),
-        Instr::CondBlock { id } => fmt!(s, "i32.eqz\nbr_if $block_{}\n", id),
+        Instr::RepeatBlock { id } => fmt!(s, "(br $loop_{})", id),
+        Instr::QuitBlock { id } => fmt!(s, "(br $block_{})", id),
+        Instr::CondBlock { id } => fmt!(s, "(br_if $block_{} (i32.eqz))", id),
         Instr::NewTuple { fields } => {
-            todo!()
+            fmt!(s, "(global.set $handy1 (call $malloc (i32.const {})))", fields.len() * 8);
+            for i in (0..fields.len()).rev() {
+                fmt!(s, "(global.get $handy1)");
+                fmt!(s, "(i64.store offset={})", i * 8);
+            }
+            fmt!(s, "(global.get $handy1)");
         }
         Instr::GetField { fields, i } => {
-            todo!()
+            fmt!(s, "(i64.load offset={})", i * 8);
         }
         Instr::SetField { fields, i } => {
-            todo!()
+            fmt!(s, "(i64.store offset={})", i * 8);
         }
-        Instr::GetLocal { id, .. } => fmt!(s, "local.get {}\n", id),
-        Instr::SetLocal { id, .. } => fmt!(s, "local.set {}\n", id),
+        Instr::GetLocal { id, .. } => fmt!(s, "(local.get {})", id),
+        Instr::SetLocal { id, .. } => fmt!(s, "(local.set {})", id),
         Instr::CallFunc { args, ret } => {
             todo!()
         }
         Instr::BindFunc { id, args, ret, capture } => {
             todo!()
         }
-        Instr::RealToInt => fmt!(s, "i64.trunc_f64_s\n"),
-        Instr::IntToReal => fmt!(s, "f64.convert_i64_s\n"),
+        Instr::RealToInt => fmt!(s, "(i64.trunc_f64_s)"),
+        Instr::IntToReal => fmt!(s, "(f64.convert_i64_s)"),
         Instr::IntToTextAscii => {
             todo!()
         }
-        Instr::NotInt => fmt!(s, "i64.const -1\ni64.xor\n"),
-        Instr::NotBool => fmt!(s, "i32.eqz\n"),
+        Instr::NotInt => fmt!(s, "(i64.const -1)(i64.xor)"),
+        Instr::NotBool => fmt!(s, "(i32.eqz)"),
         Instr::PrintText => {
             todo!()
         }
@@ -73,33 +85,33 @@ fn gen_instr(s: &mut String, instr: Instr) {
         Instr::SetArray { item } => {
             todo!()
         }
-        Instr::GetText => fmt!(s, "i32.wrap_i64\ni32.add\ni64.load8_u\n"),
+        Instr::GetText => fmt!(s, "(i32.wrap_i64)(i32.add)(i64.load8_u)"),
         Instr::CatText => {
             todo!()
         }
-        Instr::LtText => fmt!(s, "call $text_lt\n"),
-        Instr::EqText => fmt!(s, "call $text_eq\n"),
-        Instr::LenText => fmt!(s, "call $text_len\n"),
-        Instr::MulInt => fmt!(s, "i64.mul\n"),
-        Instr::MulReal => fmt!(s, "f64.add\n"),
-        Instr::DivInt => fmt!(s, "i64.div_s\n"),
-        Instr::DivReal => fmt!(s, "f64.div\n"),
-        Instr::RemInt => fmt!(s, "i64.rem_s\n"),
-        Instr::AddInt => fmt!(s, "i64.add\n"),
-        Instr::AddReal => fmt!(s, "f64.add\n"),
-        Instr::SubInt => fmt!(s, "i64.sub\n"),
-        Instr::SubReal => fmt!(s, "f64.sub\n"),
-        Instr::LtInt => fmt!(s, "i64.lt\n"),
-        Instr::EqInt => fmt!(s, "i64.eq\n"),
-        Instr::LtReal => fmt!(s, "f64.lt\n"),
-        Instr::EqReal => fmt!(s, "f64.eq\n"),
-        Instr::AndInt => fmt!(s, "i64.and\n"),
-        Instr::AndBool => fmt!(s, "i32.and\n"),
-        Instr::OrInt => fmt!(s, "i64.or\n"),
-        Instr::OrBool => fmt!(s, "i32.or\n"),
-        Instr::XorInt => fmt!(s, "i64.xor\n"),
-        Instr::XorBool => fmt!(s, "i32.xor\n"),
-        Instr::Abort => fmt!(s, "unreachable\n"),
+        Instr::LtText => fmt!(s, "(call $text_lt)"),
+        Instr::EqText => fmt!(s, "(call $text_eq)"),
+        Instr::LenText => fmt!(s, "(call $text_len)"),
+        Instr::MulInt => fmt!(s, "(i64.mul)"),
+        Instr::MulReal => fmt!(s, "(f64.add)"),
+        Instr::DivInt => fmt!(s, "(i64.div_s)"),
+        Instr::DivReal => fmt!(s, "(f64.div)"),
+        Instr::RemInt => fmt!(s, "(i64.rem_s)"),
+        Instr::AddInt => fmt!(s, "(i64.add)"),
+        Instr::AddReal => fmt!(s, "(f64.add)"),
+        Instr::SubInt => fmt!(s, "(i64.sub)"),
+        Instr::SubReal => fmt!(s, "(f64.sub)"),
+        Instr::LtInt => fmt!(s, "(i64.lt)"),
+        Instr::EqInt => fmt!(s, "(i64.eq)"),
+        Instr::LtReal => fmt!(s, "(f64.lt)"),
+        Instr::EqReal => fmt!(s, "(f64.eq)"),
+        Instr::AndInt => fmt!(s, "(i64.and)"),
+        Instr::AndBool => fmt!(s, "(i32.and)"),
+        Instr::OrInt => fmt!(s, "(i64.or)"),
+        Instr::OrBool => fmt!(s, "(i32.or)"),
+        Instr::XorInt => fmt!(s, "(i64.xor)"),
+        Instr::XorBool => fmt!(s, "(i32.xor)"),
+        Instr::Abort => fmt!(s, "(unreachable)"),
     }
 }
 
@@ -114,75 +126,65 @@ fn gen_func(s: &mut String, i: usize, code: Vec<Instr>, args: Vec<Type>, ret: Ty
     for arg in &args {
         fmt!(s, " {}", type_to_str(arg));
     }
-    fmt!(s, " i32) (result {})\n", type_to_str(&ret));
+    fmt!(s, " i32) (result {})", type_to_str(&ret));
     gen_instrs(s, code);
-    fmt!(s, ")\n");
+    fmt!(s, ")");
 
-    fmt!(s, "elem (i32.const {}) $func_{})\n", i, i);
+    fmt!(s, "elem (i32.const {}) $func_{})", i, i);
 }
 
 fn gen_program(s: &mut String, funcs: Vec<Func>, entry: usize) {
     fmt!(s, "(module
 (func $text_len (param $ptr i32) (result i64)
-    (local $len i64)
-    (local.set $len (i64.const 0))
-    (block $exit
-        (loop $loop
-            (i32.load8_u (local.get $ptr))
-            (br_if $exit (i32.eqz (i32.load8_u (local.get $ptr))))
-            (local.set $ptr (i32.add (local.get $ptr) (i32.const 1)))
-            (local.set $len (i64.add (local.get $len) (i64.const 1)))
-            (br $loop)
-        )
-    )
-    (local.get $len)
-)
+  (local $len i64)
+  (local.set $len (i64.const 0))
+  (block $exit
+    (loop $loop
+      (i32.load8_u (local.get $ptr))
+        (br_if $exit (i32.eqz (i32.load8_u (local.get $ptr))))
+        (local.set $ptr (i32.add (local.get $ptr) (i32.const 1)))
+        (local.set $len (i64.add (local.get $len) (i64.const 1)))
+        (br $loop)))
+  (local.get $len))
 (func $text_eq (param $a i32) (param $b i32) (result i32)
-    (local $charA i32)
-    (local $charB i32)
+  (local $charA i32)
+  (local $charB i32)
 
-    (block $not_equal
-      (loop $loop
-        (local.set $charA (i32.load8_u (local.get $a)))
-        (local.set $charB (i32.load8_u (local.get $b)))
+  (block $not_equal
+    (loop $loop
+      (local.set $charA (i32.load8_u (local.get $a)))
+      (local.set $charB (i32.load8_u (local.get $b)))
 
-        (br_if $not_equal (i32.ne (local.get $charA) (local.get $charB)))
-        (br_if 1 (i32.eqz (local.get $charA)))
+      (br_if $not_equal (i32.ne (local.get $charA) (local.get $charB)))
+      (br_if 1 (i32.eqz (local.get $charA)))
 
-        (local.set $a (i32.add (local.get $a) (i32.const 1)))
-        (local.set $b (i32.add (local.get $b) (i32.const 1)))
-        (br $loop)
-      )
-      (i32.const 1)
-      return
-    )
-    (i32.const 0)
-)
+      (local.set $a (i32.add (local.get $a) (i32.const 1)))
+      (local.set $b (i32.add (local.get $b) (i32.const 1)))
+      (br $loop))
+    (i32.const 1)
+    return)
+  (i32.const 0))
 (func $text_lt (param $a i32) (param $b i32) (result i32)
-    (local $charA i32)
-    (local $charB i32)
+  (local $charA i32)
+  (local $charB i32)
 
-    (block $gt_eq
-      (loop $loop
-        (local.set $charA (i32.load8_u (local.get $a)))
-        (local.set $charB (i32.load8_u (local.get $b)))
+  (block $gt_eq
+    (loop $loop
+      (local.set $charA (i32.load8_u (local.get $a)))
+      (local.set $charB (i32.load8_u (local.get $b)))
 
-        (br_if $gt_eq (i32.ge_u (local.get $charA) (local.get $charB)))
-        (br_if 1 (i32.eqz (local.get $charA)))
+      (br_if $gt_eq (i32.ge_u (local.get $charA) (local.get $charB)))
+      (br_if 1 (i32.eqz (local.get $charA)))
 
-        (local.set $a (i32.add (local.get $a) (i32.const 1)))
-        (local.set $b (i32.add (local.get $b) (i32.const 1)))
-        (br $loop)
-      )
-      (i32.const 1)
-      return
-    )
-    (i32.const 0)
-)
+      (local.set $a (i32.add (local.get $a) (i32.const 1)))
+      (local.set $b (i32.add (local.get $b) (i32.const 1)))
+      (br $loop))
+    (i32.const 1)
+    return)
+  (i32.const 0))
 
 (memory $mem 1)
 (global $heap_ptr (mut i32) (i32.const 0))
-
 (func $malloc (param $sz i64) (result i32)
     (local $size i32)
     (local $old_base i32)
@@ -216,18 +218,18 @@ fn gen_program(s: &mut String, funcs: Vec<Func>, entry: usize) {
             (unreachable)))))
 
     (global.set $heap_ptr (local.get $new_end))
-    (local.get $old_base)
-)
-\n");
+    (local.get $old_base))
 
-    fmt!(s, "(table $table {} funcref)\n", funcs.len());
+(global $handy1 (mut i32) (i32.const 0))");
+
+    fmt!(s, "(table $table {} funcref)", funcs.len());
 
     for (i, func) in funcs.into_iter().enumerate() {
         gen_func(s, i, func.code, func.args, func.ret);
     }
 
-    fmt!(s, "(start $func_{})\n", entry);
-    fmt!(s, ")\n");
+    fmt!(s, "(start $func_{})", entry);
+    fmt!(s, ")");
 }
 
 pub fn generate(program: Program) -> String {
